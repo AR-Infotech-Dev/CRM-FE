5ee22866431c938c11504070f061ba300c7da041
AR-Infotech-DEV <arinfotechtdl@gmail.com>
2026-04-27 12:07:11 +0530

    Aniket : Config for FE

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,7 @@
-export const API_BASE_URL = import.meta.env.API_BASE_URL || "http://localhost:3000/api/v1" ;
+// export const API_BASE_URL = import.meta.env.API_BASE_URL || "http://localhost:3000/api/v1" ;
+// export const API_BASE_URL = import.meta.env.API_BASE_URL || "http://192.168.1.3:3000/api/v1" ;
+export const API_BASE_URL = import.meta.env.API_BASE_URL || "https://flowups-be.onrender.com/api/v1" ;
 export const APP_NAME = import.meta.env.APP_NAME || "flowupS" ;
-// export const API_BASE_URL = import.meta.env.API_BASE_URL || "https://flowups-be.onrender.com/api/v1" ;
 
 export const DEFAULT_HEADERS = {
   "Content-Type": "application/json",
